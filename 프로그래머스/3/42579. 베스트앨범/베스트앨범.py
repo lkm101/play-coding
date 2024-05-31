de3ca19ee822e7d3e0d6ba89eaaf91490f48c688
Lee KiMok <70103652+lkm101@users.noreply.github.com>
2024-05-31 16:08:19 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,32 @@
+def solution(genres, plays):
+    answer = []
+#     playDic = {}  # {장르 : 총 재생 횟수}
+#     dic = {}      # {장르 : [(플레이 횟수, 고유번호)]}
+    
+#     # 해시 만들기
+#     for i in range(len(genres)):
+#         playDic[genres[i]] = playDic.get(genres[i], 0) + plays[i]
+#         dic[genres[i]] = dic.get(genres[i], []) + [(plays[i], i)]
+    
+#     # 재생 횟수 내림차순으로 장르별 정렬
+#     genreSort = sorted(playDic.items(), key = lambda x: x[1], reverse = True)
+#     print(genreSort)
+#     # 재생 횟수 내림차순, 인덱스 오름차순 정렬
+#     # 장르별로 최대 2개 선택
+#     for (genre, totalPlay) in genreSort:
+#         dic[genre] = sorted(dic[genre], key = lambda x: (-x[0], x[1]))
+#         answer += [idx for (play, idx) in dic[genre][:2]]
+    dic = {} 
+    genres_plays = {}
+    
+    for i in range(len(genres)):
+        genres_plays[genres[i]] = genres_plays.get(genres[i], 0) + plays[i]
+        dic[genres[i]] = dic.get(genres[i], []) + [(plays[i], i)]
+    
+    sorted_genres = sorted(genres_plays.items(), key=lambda x: x[1], reverse = True)
+    
+    for (genre, totalPlay) in sorted_genres:
+        dic[genre] = sorted(dic[genre], key = lambda x: (-x[0], x[1]))
+        answer += [idx for (play, idx) in dic[genre][:2]]
+    
+    return answer
